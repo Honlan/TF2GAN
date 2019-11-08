9e1875196ba6401a5df7a77e4aee21816d45018b
Honlan <493722771@qq.com>
2019-11-08 17:48:11 +0800

    rewrite Singan

--- SinGAN/model.py
+++ SinGAN/model.py
@@ -43,6 +43,7 @@ class Model(tk.Model):
 
 	def build_model(self):
 		if self.args.phase == 'train':
+			self.filters = [min(self.args.num_filter * np.power(2, i // 4), 128) for i in range(self.num_scale)]
 			self.Gs = []
 			self.Ds = []
 			self.noise_weights = []
@@ -51,22 +52,18 @@ class Model(tk.Model):
 			self.load_model()
 
 	def train(self):
-		filters_prev = 0
 		start_time = time.time()
-
 		for scale in range(self.num_scale):
 			img = self.imgs[scale]
 			imsave(os.path.join(self.args.save_dir, f'real_{scale}.jpg'), imdenorm(img.numpy()[0]))
 
-			filters = min(self.args.num_filter * np.power(2, scale // 4), 128)
-			h, w, c = self.sizes[scale][0], self.sizes[scale][1], self.sizes[scale][2]
-
-			if filters == filters_prev:
+			if scale > 0 and self.filters[scale] == self.filters[scale - 1]:
 				G = tk.models.load_model(os.path.join(self.args.save_dir, f'G_{scale - 1}.h5'))
 				D = tk.models.load_model(os.path.join(self.args.save_dir, f'D_{scale - 1}.h5'))
 			else:
-				G = self.generator(h, w, c, filters)
-				D = self.discriminator(h, w, c, filters)
+				h, w, c, f = self.sizes[scale][0], self.sizes[scale][1], self.sizes[scale][2], self.filters[scale]
+				G = self.generator(h, w, c, f)
+				D = self.discriminator(h, w, c, f)
 			
 			self.Gs.append(G)
 			self.Ds.append(D)
@@ -75,47 +72,55 @@ class Model(tk.Model):
 			self.optimizer_g = tk.optimizers.Adam(learning_rate=lr, beta_1=0.5)
 			self.optimizer_d = tk.optimizers.Adam(learning_rate=lr, beta_1=0.5)
 
-			rec_prev = self.get_prev('rec')
 			noise_weight = 1. if scale == 0 else self.args.noise_weight * tf.sqrt(l2_loss(img, rec_prev))
 			self.noise_weights.append(noise_weight)
 
 			for i in range(self.args.iteration):
-				z = tf.random.uniform([1, h, w, c], -1., 1.)
-				z_fixed = tf.random.uniform(tf.shape(self.imgs[0]), -1., 1.)
+				z_fixed = tf.random.normal(tf.shape(self.imgs[0]))
 
-				h0, w0, c0 = self.sizes[0][0], self.sizes[0][1], self.args.img_nc
-				self.
+				fake_prev = 0.
+				for j in range(scale):
+					z = tf.random.normal(tf.shape(self.imgs[j]))
+					fake_prev = self.Gs[j]([self.noise_weights[j] * z + fake_prev, fake_prev], training=False)
+					fake_prev = tf.image.resize(fake_prev, (self.sizes[j + 1][0], self.sizes[j + 1][1]))
 
-				for j in range(self.args.D_step):
-					with tf.GradientTape() as tape_d:
-						d_real = self.D(img, training=True)
-						
-						fake_prev = self.get_prev('random')
-						fake = self.G([noise_weight * z + fake_prev, fake_prev], training=True)
-						d_fake = self.D(fake, training=True)
-						loss_d_adv = discriminator_loss(d_real, d_fake, self.args.gan_type)
-						
-						alpha = tf.random.uniform([1, 1, 1, 1], 0., 1.)
-						inter_sample = alpha * img + (1 - alpha) * fake
-						loss_d_gp = gradient_penalty(self.D, inter_sample, self.args.w_gp)
+				rec_prev = 0.
+				for j in range(scale):
+					zr = z_fixed if j == 0 else rec_prev
+					rec_prev = self.Gs[j]([zr, rec_prev], training=False)
+					rec_prev = tf.image.resize(rec_prev, (self.sizes[j + 1][0], self.sizes[j + 1][1]))
 
-						loss_d = loss_d_adv + loss_d_gp
+				for j in range(self.args.D_step + self.args.G_step):
+					z = tf.random.normal(tf.shape(self.imgs[scale]))
 
-					self.optimizer_d.apply_gradients(zip(tape_d.gradient(loss_d, self.D.trainable_variables), self.D.trainable_variables))
+					with tf.GradientTape() as tape_d, tf.GradientTape() as tape_g:
+						if j < self.args.D_step:
+							d_real = self.Ds[scale](img, training=True)
 
-				for j in range(self.args.G_step):
-					with tf.GradientTape() as tape_g:
-						fake_prev = self.get_prev('random')
-						fake = self.G([noise_weight * z + fake_prev, fake_prev], training=True)
-						d_fake = self.D(fake, training=True)
-						loss_g_adv = generator_loss(d_fake, self.args.gan_type)
+						fake = self.Gs[scale]([noise_weight * z + fake_prev, fake_prev], training=True)
+						d_fake = self.Ds[scale](fake, training=True)
 
-						zr = noise_weight * self.z_fixed if scale == 0 else rec_prev
-						rec = self.G([zr, rec_prev], training=True)
-						loss_g_rec = self.args.w_rec * l2_loss(rec, img)
-						loss_g = loss_g_adv + loss_g_rec
+						if j < self.args.D_step:
+							loss_d_adv = discriminator_loss(d_real, d_fake, self.args.gan_type)
 
-					self.optimizer_g.apply_gradients(zip(tape_g.gradient(loss_g, self.G.trainable_variables), self.G.trainable_variables))
+							alpha = tf.random.uniform([1, 1, 1, 1], 0., 1.)
+							inter_sample = alpha * img + (1 - alpha) * fake
+							loss_d_gp = gradient_penalty(self.Ds[scale], inter_sample, self.args.w_gp)
+							
+							loss_d = loss_d_adv + loss_d_gp
+						else:
+							loss_g_adv = generator_loss(d_fake, self.args.gan_type)
+
+							zr = z_fixed if scale == 0 else rec_prev
+							rec = self.Gs[scale]([zr, rec_prev], training=True)
+							loss_g_rec = self.args.w_rec * l2_loss(rec, img)
+
+							loss_g = loss_g_adv + loss_g_rec
+
+					if j < self.args.D_step:
+						self.optimizer_d.apply_gradients(zip(tape_d.gradient(loss_d, self.Ds[scale].trainable_variables), self.Ds[scale].trainable_variables))
+					else:
+						self.optimizer_g.apply_gradients(zip(tape_g.gradient(loss_g, self.Gs[scale].trainable_variables), self.Gs[scale].trainable_variables))
 
 				print('scale: [%d/%d] iter: [%4d/%4d] time: %.2f loss_g_adv: %.4f, loss_g_rec: %.4f, loss_d_adv: %.4f, loss_d_gp: %.4f' % (
 					scale, self.num_scale, i, self.args.iteration, time.time() - start_time, loss_g_adv.numpy(), loss_g_rec.numpy(), loss_d_adv.numpy(), loss_d_gp.numpy()))
@@ -124,27 +129,7 @@ class Model(tk.Model):
 					imsave(os.path.join(self.args.save_dir, f'fake_{scale}_{i + 1}.jpg'), imdenorm(fake.numpy()[0]))
 					imsave(os.path.join(self.args.save_dir, f'rec_{scale}_{i + 1}.jpg'), imdenorm(rec.numpy()[0]))
 
-			filters_prev = filters
-			self.Gs.append(self.G)
 			self.save_model(scale)
-
-	def get_prev(self, mode='random'):
-		prev = 0.
-		
-		if mode == 'random':
-			for i in range(len(self.Gs)):
-				h, w, c = self.sizes[i][0], self.sizes[i][1], self.args.img_nc
-				z = tf.random.uniform([1, h, w, c], -1., 1.)
-				prev = self.Gs[i]([self.noise_weights[i] * z + prev, prev], training=False)
-				prev = tf.image.resize(prev, (self.sizes[i + 1][0], self.sizes[i + 1][1]))
-		
-		elif mode == 'rec':
-			for i in range(len(self.Gs)):
-				zr = self.noise_weights[i] * self.z_fixed if i == 0 else prev
-				prev = self.Gs[i]([zr, prev], training=False)
-				prev = tf.image.resize(prev, (self.sizes[i + 1][0], self.sizes[i + 1][1]))
-		
-		return prev
 
 	def test(self):
 		result = self.G(tf.random.uniform([self.args.batch_size, self.args.z_dim], -1., 1.), training=False)
@@ -157,8 +142,8 @@ class Model(tk.Model):
 			self.D = tk.models.load_model(os.path.join(self.args.save_dir, 'D.h5'))
 
 	def save_model(self, scale):
-		self.G.save(os.path.join(self.args.save_dir, f'G_{scale}.h5'))
-		self.D.save(os.path.join(self.args.save_dir, f'D_{scale}.h5'))
+		self.Gs[scale].save(os.path.join(self.args.save_dir, f'G_{scale}.h5'))
+		self.Ds[scale].save(os.path.join(self.args.save_dir, f'D_{scale}.h5'))
 
-		with open(os.path.join(self.args.save_dir, 'z_fixed.pkl'), 'wb') as fw:
-			pickle.dump([self.z_fixed.numpy(), self.noise_weights], fw)
+		with open(os.path.join(self.args.save_dir, 'noise_weights.pkl'), 'wb') as fw:
+			pickle.dump(self.noise_weights, fw)
