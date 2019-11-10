947dd977edfa9571e5685a85d4e1d964c97d4e5d
Honlan <493722771@qq.com>
2019-11-10 23:56:12 +0800

    modify dataloader of DCGAN

--- DCGAN/dataloader.py
+++ DCGAN/dataloader.py
@@ -2,23 +2,35 @@
 
 import tensorflow as tf
 from glob import glob
-import os
+import os, time
+import sys
+sys.path.append('..')
+from utils import imread
 
 class Dataloader(object):
-	def __init__(self, args):
-		self.img_paths = glob(os.path.join('dataset', args.dataset_name, '*'))
-		self.data_num = len(self.img_paths)
+	def __init__(self, args, load_data_to_memory=True):
+		self.imgs = glob(os.path.join('dataset', args.dataset_name, '*'))
+		self.dataset_size = len(self.imgs)
 		self.img_size = args.img_size
 		self.img_nc = args.img_nc
 		self.batch_size = args.batch_size
+		self.load_data_to_memory = load_data_to_memory
+
+		if self.load_data_to_memory:
+			print('Loading data to memory...')
+			t0 = time.time()
+			self.imgs = [imread(path, norm=False) for path in self.imgs]
+			print('Load finished in %.2fs...' % (time.time() - t0))
 
 		AUTOTUNE = tf.data.experimental.AUTOTUNE
-		loader = tf.data.Dataset.from_tensor_slices(self.img_paths).shuffle(self.data_num).repeat()
-		self.loader = loader.map(self.preprocess, num_parallel_calls=AUTOTUNE).batch(self.batch_size).prefetch(AUTOTUNE)
+		loader = tf.data.Dataset.from_tensor_slices(self.imgs).map(self.preprocess, num_parallel_calls=AUTOTUNE)
+		self.loader = loader.shuffle(self.dataset_size).repeat().batch(self.batch_size).prefetch(AUTOTUNE)
 
-	def preprocess(self, img_path, aug=True):
-		img_str = tf.io.read_file(img_path)
-		img = tf.cond(tf.image.is_jpeg(img_str), lambda: tf.image.decode_jpeg(img_str, channels=3), lambda: tf.image.decode_png(img_str, channels=3))
+	def preprocess(self, img, aug=True):
+		if not self.load_data_to_memory:
+			img_str = tf.io.read_file(img)
+			img = tf.cond(tf.image.is_jpeg(img_str), lambda: tf.image.decode_jpeg(img_str, channels=3), lambda: tf.image.decode_png(img_str, channels=3))
+		
 		square_size = tf.reduce_mean(tf.shape(img)[:-1])
 		img = tf.image.resize_with_crop_or_pad(img, square_size, square_size)
 		
