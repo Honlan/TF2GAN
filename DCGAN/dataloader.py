1a6a7a910058bb85df8b712833574e7c984e839c
Honlan <493722771@qq.com>
2019-11-14 23:23:52 +0800

    modify dataloader of DCGAN

--- DCGAN/dataloader.py
+++ DCGAN/dataloader.py
@@ -16,28 +16,21 @@ class Dataloader(object):
 
 		AUTOTUNE = tf.data.experimental.AUTOTUNE
 		if args.phase == 'tfrecord':
-			dataset = tf.data.Dataset.from_tensor_slices(self.img_paths).map(self.read_image, 
-				AUTOTUNE).map(tf.io.serialize_tensor)
+			dataset = tf.data.Dataset.from_tensor_slices(self.img_paths).map(tf.io.read_file, AUTOTUNE)
 			tf.data.experimental.TFRecordWriter(self.tfrecord_path).write(dataset)
 
 		elif args.phase == 'train':
-			dataset = tf.data.TFRecordDataset(self.tfrecord_path).map(self.parse_tensor, AUTOTUNE)
+			dataset = tf.data.TFRecordDataset(self.tfrecord_path).map(self.load_image, AUTOTUNE)
 			self.loader = dataset.shuffle(self.dataset_size).repeat().map(self.augmentation, 
 				AUTOTUNE).batch(self.batch_size).prefetch(AUTOTUNE)
 
-	def read_image(self, path):
-		img_str = tf.io.read_file(path)
+	def load_image(self, img_str):
 		img = tf.cond(tf.image.is_jpeg(img_str), 
 			lambda: tf.image.decode_jpeg(img_str, self.img_nc), 
 			lambda: tf.image.decode_png(img_str, self.img_nc))
 		square_size = tf.reduce_mean(tf.shape(img)[:-1])
 		img = tf.image.resize_with_crop_or_pad(img, square_size, square_size)
-		img = tf.image.resize(img, (self.img_size, self.img_size))
-		img = tf.cast(img, tf.uint8)
 		return img
-
-	def parse_tensor(self, x):
-		return tf.reshape(tf.io.parse_tensor(x, tf.float32), [self.img_size, self.img_size, self.img_nc])
 
 	def augmentation(self, img):
 		img = tf.image.resize(img, (self.aug_size, self.aug_size))
